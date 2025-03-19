525e48c390a0d92bf38b63a862267c3c661052af
lauramesquitabruel <lauramesquitabruel@gmail.com>
2025-03-19 11:19:18 -0300

    incio do TDD Event

--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -1,7 +1,38 @@
 package com.devsuperior.demo.services;
 
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.entities.Event;
+import com.devsuperior.demo.repositories.CityRepository;
+import com.devsuperior.demo.repositories.EventRepository;
+import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EventService {
+    @Autowired
+    private EventRepository repository;
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional(readOnly = true)
+    public EventDTO update(Long id, EventDTO dto){
+        try{
+            Event event = repository.getReferenceById(id);
+            event.setName(dto.getName());
+            event.setDate(dto.getDate());
+            event.setUrl(dto.getUrl());
+
+            City city = cityRepository.getReferenceById(dto.getCityId());
+            event.setCity(city);
+
+            event = repository.save(event);
+            return new EventDTO(event);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Id não encontrado " + id);
+        }
+    }
 }
